1b95bd49f3c9d61ef34702222e3ac1dbecfdc412
Kishinevsv <Kishinevsv@gmail.com>
2024-11-25 14:46:18 +0300

    Some change for new IP-address.

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -4,7 +4,7 @@ import {createApi} from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://10.20.0.15:8888/',
+        baseUrl: 'http://127.0.0.1:8888/',
         prepareHeaders: (headers) => headers,
     }),
     endpoints: () => ({}),
