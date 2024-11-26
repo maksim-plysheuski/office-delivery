ea885c899dab9ce2b235116fc09ed2801820b357
Maksim Plysheuski <m.plyshevski@complitech.org>
2024-11-26 14:53:46 +0300

    v1.0.5

--- src/components/MapPage/MapPage.tsx
+++ src/components/MapPage/MapPage.tsx
@@ -33,7 +33,7 @@ export const MapPage = () => {
 
     const [createCarTask] = useCreateCarTaskMutation()
     const {data: carPositionData} = useGetCarPositionQuery(undefined, {
-        pollingInterval: 1000,
+        pollingInterval: 500,
         skip: false,
     });
 
