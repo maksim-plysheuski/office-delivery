6f748299f60eb9819ccee3bad214bd0cdc5e2d62
Maksim Plysheuski <m.plyshevski@complitech.org>
2024-11-26 15:09:34 +0300

    v1.0.7

--- src/components/MapPage/MapPage.tsx
+++ src/components/MapPage/MapPage.tsx
@@ -44,6 +44,7 @@ export const MapPage = () => {
         return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
     };
 
+    console.log('crocodile', movingMarker)
 
     useEffect(() => {
         if (carPositionData) {
